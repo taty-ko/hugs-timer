579cf6bc9f734b61ea259a83af1fc1c45d663b5e
taty-ko <hrkolesnik@gmail.com>
2022-06-23 19:49:08 +0300

    Timer added

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+const minutes = document.querySelector('#min'),
+      seconds = document.querySelector('#sec'), 
+      start = document.querySelector('.start'),
+      pause = document.querySelector('.pause'),
+      stop = document.querySelector('.stop');
+
+      
+const timerUpdate = setInterval(function() {
+        console.log(seconds);
+        console.log(minutes);
+        }, 1000);
+
+start.addEventListener('click', () => {
+    console.log('btn start');
+});
+
+ stop.addEventListener('click', () => {
+    console.log('btn stop');
+});
+
+pause.addEventListener('click', () => {
+    console.log('btn pause');
+}); 
+
+
+
+
+
+
+
+
+
+let input = document.querySelector('input');
+
+console.log(input);
+
+});
+
+
