c6f580c5634a0203be4dd475f2f8701b11b205fe
taty-ko <hrkolesnik@gmail.com>
2022-06-24 18:58:02 +0300

    Could link btns and input. yyyyhooooo

--- script.js
+++ script.js
@@ -6,33 +6,53 @@ const minutes = document.querySelector('#min'),
       seconds = document.querySelector('#sec'), 
       start = document.querySelector('.start'),
       pause = document.querySelector('.pause'),
-      stop = document.querySelector('.stop');
+      stop = document.querySelector('.stop'), 
+      inputTime = document.getElementById('getTime');
 
       
-const timerUpdate = setInterval(function() {
+/* const timerUpdate = setInterval(function() {
         console.log(seconds);
         console.log(minutes);
         }, 1000);
+ */
 
 
 
+start.addEventListener('click', function getValue(event) {
 
-start.addEventListener('click', () => {
-    console.log('btn start');
+    const val = document.getElementById('getTime');
+    
+    // Send value to server
+    console.log(val.value);
 });
 
- stop.addEventListener('click', () => {
-    console.log('btn stop');
+ stop.addEventListener('click', function stopValue(event) {
+    const val = document.getElementById('getTime');
+     val.value = "set time again";
+     console.log(val.value);
 });
 
 pause.addEventListener('click', () => {
     console.log('btn pause');
 }); 
 
+  inputTime.addEventListener('click', function getValue(event) {
+  
+    const val = document.getElementById('getTime');
+    
+    // Send value to server
+    console.log(val.value);
 
-let input = document.querySelector('input');
+  
+    /* // 👇️ clear input field
+   val.placeholder = ''; */
 
-console.log(input);
+  }); 
+
+/* inputTime.addEventListener('click', function getValue() {
+    const val = document.getElementById("getTime").value;
+    console.log(val);
+}) */
 
 });
 
